783f1eb3f07eb4be41edd8912df9de59824a5ad2
L <L@LtekiMac-mini.local>
2014-02-26 14:49:22 +0800

    Initial Commit

---
+++ MKNetworkKitDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MKNetworkKitDemo
+//
+//  Created by L on 14-2-26.
+//  Copyright (c) 2014年 YYF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MKNetworkKitDemoTests/MKNetworkKitDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  MKNetworkKitDemoTests.m
+//  MKNetworkKitDemoTests
+//
+//  Created by L on 14-2-26.
+//  Copyright (c) 2014年 YYF. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MKNetworkKitDemoTests : XCTestCase
+
+@end
+
+@implementation MKNetworkKitDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
